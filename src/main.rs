33607727734613160e380eeb594836d7bc59e21c
qazplm807733@gmail.com <qazplm807733@gmail.com>
2023-03-28 23:15:53 +0900

    fail to left_join

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,23 @@ use polars::prelude::*;
 fn main() {
     let ignore: Vec<String> = vec!["NA".to_string()];
 
-    let df = CsvReader::from_path("/home/aoqk4/rust_data/csv/athlete_events.csv")
+    let athletes = CsvReader::from_path("/home/aoqk4/rust_data/csv/athlete_events.csv")
         .unwrap()
         .with_null_values(Some(NullValues::AllColumns(ignore)))
         .finish()
         .unwrap();
 
-    println!("{:?}", df.head(Some(5)));
+    let regions = CsvReader::from_path("/home/aoqk4/rust_data/csv/noc_regions.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
 
-    let filt = df.column("Medal").unwrap().is_not_null();
+    // TODO---------------------- left_join 해보자.. -----------------------------------------------------------------------
 
-    let res = df.filter(&filt).unwrap();
+    let mut athletes_merge = athletes.left_join(&regions, ["NOC"], ["NOC"]).unwrap();
 
-    println!("{:?}", res.head(Some(5)));
+    let mut file = std::fs::File::create("path.csv").unwrap();
+    CsvWriter::new(&mut file)
+        .finish(&mut athletes_merge)
+        .unwrap();
 }
